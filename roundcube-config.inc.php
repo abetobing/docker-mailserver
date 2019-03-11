9996fcc4feef785e00a6a3ce00072bbb1ea304db
Abe Tobing <abedzilla@gmail.com>
2019-03-11 18:25:55 +0700

    Version 0.98 almost final

--- roundcube-config.inc.php
+++ roundcube-config.inc.php
@@ -1,5 +1,25 @@
+// skin name: folder from skins/
+$config['skin'] = 'larry';
 $config['db_dsnw'] = 'mysql://roundcube:roundpasswd@172.17.0.5/roundcubedb';
-$config['default_host'] = '127.0.0.1';
+#$config['default_host'] = '127.0.0.1';
 $config['smtp_server'] = 'tls://127.0.0.1';
 $config['smtp_user'] = '%u';
 $config['smtp_pass'] = '%p';
+
+#$config['default_host'] = 'tls://%n';
+#$config['default_host'] = 'tls://127.0.0.1';
+$config['default_host'] = 'abetobing.com:9143';
+
+$config['imap_conn_options'] = array(
+        'ssl' => array(
+                'verify_peer' => false,
+                'verfify_peer_name' => false,
+        ),
+);
+
+$config['smtp_conn_options'] = array(
+        'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+        ),
+);
