4d7acfd55320582a7f70211bb754580856b33dcd
Abe Tobing <abedzilla@gmail.com>
2019-03-12 18:28:06 +0700

    Enable mail_crypt and fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
-if [ "$1" = 'postfix start' ]; then
-	postfix start
+#if [ "$1" = 'postfix start' ]; then
+	service postfix start
 	service dovecot restart
 	service apache2 restart
-fi
+#fi
+
+exec >/log/stdout.log
+exec 2>/log/stderr.log
 
 exec "$@"
 
